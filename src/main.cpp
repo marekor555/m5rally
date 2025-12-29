d90d711e220e6f2e47ef9eccd167ceb8167cfc65
MAREKOR555 <MAREKOR555@INTERIA.PL>
2025-12-29 18:50:13 +0100

    add main menu

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,24 @@
 bool justPressed = false;
 
 std::vector<String> options = {
-	"t - test level",
-	"f - first level",
-	"s - enter sleep",
+	"test level",
+	"first level",
+	"enter sleep",
 };
 
+void enterSleep() {
+	M5Cardputer.Lcd.fillScreen(TFT_BLACK);
+	while (M5Cardputer.Keyboard.isPressed()) M5Cardputer.update();
+	while (!M5Cardputer.Keyboard.isPressed()) M5Cardputer.update();
+}
+
+void (*results[])() = {
+	testLevel,
+	first,
+	enterSleep,
+};
+
+int option = 0;
 void setup() {
 	M5Cardputer.begin();
 	Serial.begin(9600);
@@ -26,24 +39,38 @@ void loop() {
 		const auto status = M5Cardputer.Keyboard.keysState();
 		for (const char key: status.word) {
 			switch (key) {
-				case 't':
-					testLevel();
-				case 'f':
-					first();
+				case ';':
+					option--;
+					if (option < 0) option = options.size() - 1;
+					break;
+				case '.':
+					option++;
+					if (option >= options.size()) option = 0;
+					break;
+				case '/':
+					results[option]();
 			}
 		}
+		while (M5Cardputer.Keyboard.isPressed()) M5Cardputer.update();
 	}
 	M5Canvas canvas(&M5Cardputer.Lcd);
 	canvas.createSprite(240, 135);
 	canvas.fillScreen(TFT_BLACK);
 	canvas.setTextColor(TFT_WHITE, TFT_BLACK);
-	canvas.setTextSize(1);
+	int previous = option - 1;
+	if (previous < 0) previous = options.size() - 1;
+	if (previous >= options.size()) previous = 0;
+	int next = option + 1;
+	if (next < 0) next = options.size() - 1;
+	if (next >= options.size()) next = 0;
 
-	for (const String &option : options) {
-		canvas.println(option);
-	}
+	canvas.setTextSize(2);
+	canvas.drawCenterString(options[option], M5Cardputer.Lcd.width()/2, M5Cardputer.Lcd.height()/2);
+
+	canvas.setTextSize(1);
+	canvas.drawCenterString(options[previous], M5Cardputer.Lcd.width()/2, M5Cardputer.Lcd.height()*0.25);
+	canvas.drawCenterString(options[next], M5Cardputer.Lcd.width()/2, M5Cardputer.Lcd.height()*0.75);
 
 	canvas.pushSprite(0, 0);
 	canvas.deleteSprite();
-	delay(17);  // ~60fps
 }
