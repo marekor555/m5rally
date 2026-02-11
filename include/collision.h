8c9d95ebfbb9d7be5d86ea398c729f77e5219558
MAREKOR555 <MAREKOR555@INTERIA.PL>
2026-02-11 17:55:52 +0100

    add some docs

--- include/collision.h
+++ include/collision.h
@@ -2,8 +2,8 @@
 // Created by marekor on 12.12.2025.
 //
 
-#ifndef M5RALLY_COLLISON_H
-#define M5RALLY_COLLISON_H
+#ifndef M5RALLY_COLLISION_H
+#define M5RALLY_COLLISION_H
 
 #include <M5Cardputer.h>
 
@@ -16,24 +16,32 @@ public:
 	float x;
 	float y;
 
-	Pos2D(float ix = 0, float iy = 0) : x(ix), y(iy) {
+	Pos2D(const float ix = 0, const float iy = 0) : x(ix), y(iy) {
 	}
 };
 
 class NGon {
 public:
 	uint16_t color;
-	uint16_t infillColor;
+	uint16_t infillColor{};
 	std::vector<Pos2D> corners;
 
-	NGon(uint16_t color) : color(color) {
+	NGon(const uint16_t color) : color(color) {
 	}
-	NGon(uint16_t color, uint16_t infillColor) : color(color), infillColor(infillColor) {
-	}
-
-	NGon() : color(0), infillColor(0) {
+	NGon(const uint16_t color, const uint16_t infillColor) : color(color), infillColor(infillColor) {
 	}
 
+	NGon() : color(0) {
+	}
+
+	/**
+	 * convert rectangle into NGon format
+	 * @param centerX center x coordinate of the rectangle
+	 * @param centerY center y coordinate of the rectangle
+	 * @param width width of the rectangle
+	 * @param height height of the rectangle
+	 * @param angle angle of rotation (clockwise from up)
+	 */
 	void fromRectangle(float centerX, float centerY, float width, float height, float angle);
 
 	void drawOutline(M5Canvas *display, float camposX, float camposY) const;
@@ -45,7 +53,7 @@ public:
 	uint16_t color;
 	std::vector<Pos2D> points;
 
-	Line(uint16_t color) : color(color) {
+	Line(const uint16_t color) : color(color) {
 	}
 
 	Line() : color(0) {
@@ -60,6 +68,9 @@ bool NGonCollision(const NGon &n1, const NGon &n2);
 
 bool NGonLineCollision(const NGon &n, const Line &l);
 
+/**
+ * check if rectangles collide (line intersection)
+ */
 bool collision(float x1, float y1, float w1, float h1, float a1,
 				float x2, float y2, float w2, float h2, float a2);
-#endif  // M5RALLY_COLLISON_H
+#endif  // M5RALLY_COLLISION_H
--- include/utils.h
+++ include/utils.h
@@ -7,14 +7,49 @@
 #include <Arduino.h>
 #include <vector>
 
+/*
+ * wait for all keys to be released
+ */
 void debounceKeyboard();
 
+/**
+ * gui part for scrolling through many string options
+ * @param msg array of options
+ * @param scrollX choose to enable/disable x axis scroll
+ * @param mainColor main text color
+ * @param extraColor color of special options, options with | at start and end
+ * @return text of the selected option
+ */
 String scrollTextArrHighlight(const std::vector<String> &msg, bool scrollX, int mainColor, int extraColor);
 
+/**
+ * gui part for scrolling through many function options
+ * @param descriptions function descriptions
+ * @param functions function definitions
+ */
 void selectFunction(const std::vector<String> &descriptions, void (*functions[])());
 
+/**
+ * check if a point is visible on the screen
+ * @param x point x coordinate
+ * @param y point y coordinate
+ * @param camposX camera x coordinate
+ * @param camposY camera y coordinate
+ * @param margin margin for detection (in pixels)
+ * @return
+ */
 bool isVisible(float x, float y, float camposX, float camposY, float margin);
 
+/**
+ * check if a line is visible on the screen
+ * @param x1 point 1 x coordinate
+ * @param y1 point 1 y coordinate
+ * @param x2 point 2 x coordinate
+ * @param y2 point 2 y coordinate
+ * @param camposX camera x coordinate
+ * @param camposY camera y coordinate
+ * @return
+ */
 bool intersectScreen(float x1, float y1, float x2, float y2, float camposX, float camposY);
 
 #endif //M5RALLY_UTILS_H
